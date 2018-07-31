7266af653ffc6ff6a4115ae58a9379af82ca62d5
Prabakaran <Prabakaran1993@hotmail.com>
2018-07-31 22:45:47 +0530

    #1:Base Cli

---
+++ Pluralsight.go
@@ -0,0 +1,253 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type authStruct struct {
+	DeviceID     string `json:"deviceId"`
+	RefreshToken string `json:"refreshToken"`
+}
+
+type tokenStruct struct {
+	Token         string    `json:"token"`
+	Jwt           string    `json:"jwt"`
+	UserHandle    string    `json:"userHandle"`
+	Expiration    time.Time `json:"expiration"`
+	Authenticated bool      `json:"authenticated"`
+	OfflineAccess struct {
+		ViewOffline bool `json:"viewOffline"`
+		CacheDays   int  `json:"cacheDays"`
+		CourseLimit int  `json:"courseLimit"`
+	} `json:"offlineAccess"`
+}
+
+type videoMetaDataStruct struct {
+	Data struct {
+		ViewClip struct {
+			Urls []struct {
+				URL    string      `json:"url"`
+				Cdn    string      `json:"cdn"`
+				Rank   int         `json:"rank"`
+				Source interface{} `json:"source"`
+			} `json:"urls"`
+			Status int `json:"status"`
+		} `json:"viewClip"`
+	} `json:"data"`
+}
+
+type playListsStruct struct {
+	Data struct {
+		RPC struct {
+			BootstrapPlayer struct {
+				Course struct {
+					Name                           string `json:"name"`
+					Title                          string `json:"title"`
+					CourseHasCaptions              bool   `json:"courseHasCaptions"`
+					SupportsWideScreenVideoFormats bool   `json:"supportsWideScreenVideoFormats"`
+					Timestamp                      string `json:"timestamp"`
+					Modules                        []struct {
+						Name              string `json:"name"`
+						Title             string `json:"title"`
+						Duration          int    `json:"duration"`
+						FormattedDuration string `json:"formattedDuration"`
+						Author            string `json:"author"`
+						Authorized        bool   `json:"authorized"`
+						Clips             []struct {
+							Authorized        bool   `json:"authorized"`
+							ClipID            string `json:"clipId"`
+							Duration          int    `json:"duration"`
+							FormattedDuration string `json:"formattedDuration"`
+							ID                string `json:"id"`
+							Index             int    `json:"index"`
+							ModuleIndex       int    `json:"moduleIndex"`
+							ModuleTitle       string `json:"moduleTitle"`
+							Name              string `json:"name"`
+							Title             string `json:"title"`
+							Watched           bool   `json:"watched"`
+						} `json:"clips"`
+					} `json:"modules"`
+				} `json:"course"`
+			} `json:"bootstrapPlayer"`
+		} `json:"rpc"`
+	} `json:"data"`
+}
+
+func OneGetAuth(email string, password string) *authStruct {
+	var cdnurl = "https://app.pluralsight.com/mobile-api/v2/user/device/authenticated"
+	client := &http.Client{}
+	query := "{\"DeviceModel\":\"Windows Desktop\",\"DeviceName\":null,\"Username\":\"" + email + "\",\"Password\":\"" + password + "\"}"
+	var jsonStr = []byte(query)
+	req, _ := http.NewRequest("POST", cdnurl, bytes.NewBuffer(jsonStr))
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
+	req.Header.Set("content-type", "application/json;charset=UTF-8")
+	resp, _ := client.Do(req)
+	if resp.StatusCode != 200 {
+		fmt.Fprintf(os.Stderr, "Authorization Failed")
+		os.Exit(1)
+	}
+	_authStruct := new(authStruct)
+	json.NewDecoder(resp.Body).Decode(_authStruct)
+	// fmt.Println(_authStruct.DeviceID)
+	defer resp.Body.Close()
+	return _authStruct
+}
+
+func TwoGetToken(_authStruct *authStruct) *tokenStruct {
+	var cdnurl = "https://app.pluralsight.com/mobile-api/v2/user/authorization/" + _authStruct.DeviceID
+	client := &http.Client{}
+	_authStructJson, _ := json.Marshal(_authStruct)
+	var jsonStr = []byte(_authStructJson)
+	req, _ := http.NewRequest("POST", cdnurl, bytes.NewBuffer(jsonStr))
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
+	req.Header.Set("content-type", "application/json;charset=UTF-8")
+	resp, _ := client.Do(req)
+	_tokenStruct := new(tokenStruct)
+	json.NewDecoder(resp.Body).Decode(_tokenStruct)
+	// fmt.Println(_tokenStruct.Jwt)
+	defer resp.Body.Close()
+	return _tokenStruct
+}
+
+func ThreeGetPlayLists(_tokenStruct *tokenStruct, course_name string) *playListsStruct {
+	var cdnurl = "https://app.pluralsight.com/player/api/graphql"
+	var _query = "{\"query\":\"query BootstrapPlayer {rpc{bootstrapPlayer{course(courseId: \\\"" + course_name + "\\\") {        name title courseHasCaptions   supportsWideScreenVideoFormats timestamp modules {   name   title   duration   formattedDuration   author   authorized   clips {     authorized     clipId     duration     formattedDuration     id     index     moduleIndex     moduleTitle     name     title      watched }}}}}}\"}"
+	// fmt.Println(_query)
+	client := &http.Client{}
+	var jsonStr = []byte(_query)
+	req, _ := http.NewRequest("POST", cdnurl, bytes.NewBuffer(jsonStr))
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("cookie", "PsJwt-production="+_tokenStruct.Jwt+";")
+	resp, _ := client.Do(req)
+	_playListsStruct := new(playListsStruct)
+	json.NewDecoder(resp.Body).Decode(_playListsStruct)
+	defer resp.Body.Close()
+	return _playListsStruct
+}
+
+func FourGetVideoMetadata(_tokenStruct *tokenStruct, id string) string {
+	time.Sleep(1 * time.Second)
+	var cdnurl = "https://app.pluralsight.com/player/api/graphql"
+	client := &http.Client{}
+	//"go-cli-playbook:45c18713-161d-4455-a309-56507c151581:3:mike-vansickle"
+	course_name := strings.Split(id, ":")[0]
+	clip_id := strings.Split(id, ":")[1]
+	clip_index := strings.Split(id, ":")[2]
+	author := strings.Split(id, ":")[3]
+	query := "{\"query\":\" query viewClip {  viewClip(input: {    author: \\\"" + author + "\\\",     clipIndex: " + clip_index + ",     courseName: \\\"" + course_name + "\\\",     includeCaptions: false,     locale: \\\"en\\\",     mediaType: \\\"mp4\\\",     moduleName: \\\"" + clip_id + "\\\",     quality: \\\"1280x720\\\"  }) {    urls {      url      cdn      rank      source    },    status  }        }      \"}"
+	var jsonStr = []byte(query)
+	req, _ := http.NewRequest("POST", cdnurl, bytes.NewBuffer(jsonStr))
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
+	req.Header.Set("content-type", "application/json;charset=UTF-8")
+	req.Header.Set("cookie", "PsJwt-production="+_tokenStruct.Jwt+";")
+	resp, _ := client.Do(req)
+	_videoMetaData := new(videoMetaDataStruct)
+	json.NewDecoder(resp.Body).Decode(_videoMetaData)
+	//fmt.Println("Status Code: ", resp.StatusCode)
+	//fmt.Println(_videoMetaData.Data.ViewClip.Urls)
+	if _videoMetaData.Data.ViewClip.Status == 429 {
+		fmt.Println("Status Code: ", _videoMetaData.Data.ViewClip)
+		htmlData, err := ioutil.ReadAll(resp.Body) //<--- here!
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(string(htmlData)) //<-- here !
+		return strconv.Itoa(_videoMetaData.Data.ViewClip.Status) + " status"
+	}
+	defer resp.Body.Close()
+	return _videoMetaData.Data.ViewClip.Urls[0].URL
+}
+
+func worker(i int, jobs <-chan list, results chan<- string) {
+	for j := range jobs {
+		// fmt.Println("worker", i, "started  job", j.id)
+		client := &http.Client{}
+		req, _ := http.NewRequest("GET", j.url, nil)
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
+		resp, _ := client.Do(req)
+		// fmt.Println(resp.StatusCode)
+		defer resp.Body.Close()
+		reader, _ := ioutil.ReadAll(resp.Body)
+		ioutil.WriteFile(j.fileName, []byte(string(reader)), 0x777) // Write to the file i as a byte array
+		resp.Body.Close()
+		//fmt.Println("worker", i, "finished job", j.id)
+		results <- j.fileName
+	}
+}
+
+type list struct {
+	fileName string `json:"fileName"`
+	url      string `json:"url"`
+}
+
+var name = flag.String("name", "", "Enter the username")
+var password = flag.String("password", "", "Enter the password")
+var playlisturl = flag.String("playlisturl", "", "Enter the playlist url")
+
+func main() {
+	flag.Parse()
+	_authStruct := OneGetAuth(*name, *password)
+	u, _ := url.Parse(*playlisturl)
+	fragments, _ := url.ParseQuery(u.RawQuery)
+	_tokenStruct := TwoGetToken(_authStruct)
+	_playListsStruct := ThreeGetPlayLists(_tokenStruct, fragments["course"][0])
+	modules := _playListsStruct.Data.RPC.BootstrapPlayer.Course.Modules
+	_list := make([]list, 0)
+	for i := 0; i < len(modules); i++ {
+		module := modules[i]
+		for j := 0; j < len(module.Clips); j++ {
+			//fmt.Println(module.Clips[j])
+			name := strconv.Itoa(i) + "_" + strings.Replace(module.Title, " ", "_", -1) + "_" + strconv.Itoa(j) + "_" + strings.Replace(module.Clips[j].Title, " ", "_", -1) + ".mp4"
+			name = strings.Replace(name, ":", "", -1)
+			name = strings.Replace(name, "'", "", -1)
+			url := FourGetVideoMetadata(_tokenStruct, module.Clips[j].ID)
+			fmt.Println("Processing MetaData for:", name)
+			_list = append(_list, list{fileName: name, url: url})
+			// break
+		}
+		// break
+	}
+	workers := 2
+	fmt.Println("Workers: ", workers)
+	length := len(_list)
+	jobs := make(chan list)
+	results := make(chan string, length+1)
+	for i := 0; i < workers; i++ {
+		go worker(i, jobs, results)
+	}
+	for i := 0; i < length; i++ {
+		jobs <- _list[i]
+	}
+	close(jobs)
+	// Finally we collect all the results of the work.
+	for a := 0; a < length; a++ {
+		fmt.Println("Download : ", <-results)
+	}
+}
+
+// fmt.Println("Status Code: ", resp.StatusCode)
+// htmlData, err := ioutil.ReadAll(resp.Body) //<--- here!
+// if err != nil {
+// 	fmt.Println(err)
+// 	os.Exit(1)
+// }
+// fmt.Println(string(htmlData)) //<-- here !
+
+// listJson, _ := json.Marshal(_list)
+// ioutil.WriteFile(_playListsStruct.Data.RPC.BootstrapPlayer.Course.Name, listJson, 0644)
+
+// values := map[string]string{"username": username, "password": password}
+// jsonValue, _ := json.Marshal(values)
+// resp, err := http.Post(authAuthenticatorUrl, "application/json", bytes.NewBuffer(jsonValue))
---
+++ Readme.md
@@ -0,0 +1,83 @@
+``Pluralsight Downloader``
+````
+go build Pluralsight.go
+Pluralsight.exe --name=demo@gmail.com --password=demo123 --playlisturl=https://app.pluralsight.com/player?course=go-cli-playbook&author=mike-vansickle&name=45c18713-161d-4455-a309-56507c151581&clip=3&mode=live
+Processing MetaData for: 0_Course_Overview_0_Course_Overview.mp4
+Processing MetaData for: 1_Introduction_0_Introduction.mp4
+Processing MetaData for: 1_Introduction_1_The_Go_Command.mp4
+Processing MetaData for: 1_Introduction_2_Demo_The_Go_Command.mp4
+Processing MetaData for: 1_Introduction_3_Navigating_This_Course.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_0_Introduction.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_1_Demo_The_Run_Command.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_2_Demo_The_Build_Command.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_3_Demo_The_Install_Command.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_4_Demo_Buildflags.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_5_Demo_Race_Detection.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_6_Demo_Building_Shared_Libraries.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_7_Demo_Statically_Linking_to_C_Programs.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_8_Demo_Dynamically_Link_to_C_Programs.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_9_Demo_Building_Plugins.mp4
+Processing MetaData for: 2_Building_and_Running_Programs_10_Summary.mp4
+Processing MetaData for: 3_Testing_Programs_0_Introduction.mp4
+Processing MetaData for: 3_Testing_Programs_1_Demo_Running_Basic_Tests.mp4
+Processing MetaData for: 3_Testing_Programs_2_Demo_Using_Test_Flags.mp4
+Processing MetaData for: 3_Testing_Programs_3_Demo_Generating_Code_Coverage_Reports.mp4
+Processing MetaData for: 3_Testing_Programs_4_Demo_Running_Benchmarking_Tests.mp4
+Processing MetaData for: 3_Testing_Programs_5_Demo_Generating_Performance_Profiles.mp4
+Processing MetaData for: 3_Testing_Programs_6_Summary.mp4
+Processing MetaData for: 4_Managing_Workspaces_0_Introduction.mp4
+Processing MetaData for: 4_Managing_Workspaces_1_Demo_Cleaning_Workspaces.mp4
+Processing MetaData for: 4_Managing_Workspaces_2_Demo_Formatting_Source_Code.mp4
+Processing MetaData for: 4_Managing_Workspaces_3_Demo_Vetting_Source_Code.mp4
+Processing MetaData for: 4_Managing_Workspaces_4_Demo_Generating_Source_Code.mp4
+Processing MetaData for: 4_Managing_Workspaces_5_Summary.mp4
+Processing MetaData for: 5_Interacting_with_the_Environment_0_Introduction.mp4
+Processing MetaData for: 5_Interacting_with_the_Environment_1_Demo_Finding_Gos_Version.mp4
+Processing MetaData for: 5_Interacting_with_the_Environment_2_Demo_Exploring_Environment_Settings.mp4
+Processing MetaData for: 5_Interacting_with_the_Environment_3_Demo_Listing_Packages.mp4
+Processing MetaData for: 5_Interacting_with_the_Environment_4_Demo_Viewing_Documentation.mp4
+Processing MetaData for: 5_Interacting_with_the_Environment_5_Demo_Getting_Packages.mp4
+Processing MetaData for: 5_Interacting_with_the_Environment_6_Demo_Reporting_Bugs.mp4
+Processing MetaData for: 5_Interacting_with_the_Environment_7_Summary.mp4
+Workers:  2
+Download :  1_Introduction_0_Introduction.mp4
+Download :  0_Course_Overview_0_Course_Overview.mp4
+Download :  1_Introduction_2_Demo_The_Go_Command.mp4
+Download :  1_Introduction_3_Navigating_This_Course.mp4
+Download :  1_Introduction_1_The_Go_Command.mp4
+Download :  2_Building_and_Running_Programs_0_Introduction.mp4
+Download :  2_Building_and_Running_Programs_1_Demo_The_Run_Command.mp4
+Download :  2_Building_and_Running_Programs_2_Demo_The_Build_Command.mp4
+Download :  2_Building_and_Running_Programs_3_Demo_The_Install_Command.mp4
+Download :  2_Building_and_Running_Programs_5_Demo_Race_Detection.mp4
+Download :  2_Building_and_Running_Programs_4_Demo_Buildflags.mp4
+Download :  2_Building_and_Running_Programs_6_Demo_Building_Shared_Libraries.mp4
+Download :  2_Building_and_Running_Programs_7_Demo_Statically_Linking_to_C_Programs.mp4
+Download :  2_Building_and_Running_Programs_8_Demo_Dynamically_Link_to_C_Programs.mp4
+Download :  2_Building_and_Running_Programs_10_Summary.mp4
+Download :  3_Testing_Programs_0_Introduction.mp4
+Download :  3_Testing_Programs_1_Demo_Running_Basic_Tests.mp4
+Download :  3_Testing_Programs_2_Demo_Using_Test_Flags.mp4
+Download :  3_Testing_Programs_3_Demo_Generating_Code_Coverage_Reports.mp4
+Download :  3_Testing_Programs_4_Demo_Running_Benchmarking_Tests.mp4
+Download :  2_Building_and_Running_Programs_9_Demo_Building_Plugins.mp4
+Download :  3_Testing_Programs_5_Demo_Generating_Performance_Profiles.mp4
+Download :  4_Managing_Workspaces_0_Introduction.mp4
+Download :  3_Testing_Programs_6_Summary.mp4
+Download :  4_Managing_Workspaces_2_Demo_Formatting_Source_Code.mp4
+Download :  4_Managing_Workspaces_3_Demo_Vetting_Source_Code.mp4
+Download :  4_Managing_Workspaces_4_Demo_Generating_Source_Code.mp4
+Download :  4_Managing_Workspaces_5_Summary.mp4
+Download :  5_Interacting_with_the_Environment_0_Introduction.mp4
+Download :  5_Interacting_with_the_Environment_1_Demo_Finding_Gos_Version.mp4
+Download :  5_Interacting_with_the_Environment_2_Demo_Exploring_Environment_Settings.mp4
+Download :  5_Interacting_with_the_Environment_3_Demo_Listing_Packages.mp4
+Download :  4_Managing_Workspaces_1_Demo_Cleaning_Workspaces.mp4
+Download :  5_Interacting_with_the_Environment_4_Demo_Viewing_Documentation.mp4
+Download :  5_Interacting_with_the_Environment_5_Demo_Getting_Packages.mp4
+Download :  5_Interacting_with_the_Environment_6_Demo_Reporting_Bugs.mp4
+Download :  5_Interacting_with_the_Environment_7_Summary.mp4
+````
+
+``Note``
+purely doing for Research purpose
